5ea933df3d0f1d339f07decb589c76d21844dcb7
Mattia Biondi <mattiabiondi1@gmail.com>
2020-11-20 15:36:27 +0100

    Change mouse and keyboard usb ports

--- scripts/powertop-fix.sh
+++ scripts/powertop-fix.sh
@@ -2,8 +2,8 @@
 
 sleep 1;
 
-MOUSE="/sys/bus/usb/devices/1-5.1.4.2/power/control";
-KEYBOARD="/sys/bus/usb/devices/1-5.3/power/control"
+MOUSE="/sys/bus/usb/devices/1-5.3.3/power/control"
+KEYBOARD="/sys/bus/usb/devices/1-5.3.2/power/control"
 
 if [ -f "$MOUSE" ]; then
 	echo 'on' > $MOUSE;
